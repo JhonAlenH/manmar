401318f26b0f0855edf8f5b4c8935aaf1f252be9
Andres Quintero <quand.mind@gmail.com>
2026-02-18 16:36:25 -0400

    fix

--- src/app/components/maestros/maestros.component.ts
+++ src/app/components/maestros/maestros.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaestrosComponent implements OnInit {
   public isCollapsed = true;
+  currentUser:any
   constructor() { }
   menuItems: any = [
     {
@@ -27,7 +28,6 @@ export class MaestrosComponent implements OnInit {
       text: 'Personas', icon: 'fa-solid fa-user' , id: 'personas', items: [
         {text: 'Clientes', icon: 'fa-solid fa-id-card', url: 'clientes'},
         {text: 'Productores', icon: 'fa-solid fa-people-arrows', url: 'productores'},
-        {text: 'Usuarios', icon: 'fa-solid fa-circle-user', url: 'usuarios'},
       ]
     },
     {
@@ -41,6 +41,43 @@ export class MaestrosComponent implements OnInit {
   ]
 
   ngOnInit(): void {
+    const storedSession = localStorage.getItem('user');
+    this.currentUser = JSON.parse(storedSession);
+    this.currentUser = this.currentUser.data.user
+    console.log(this.currentUser)
+    if([1,2,3].includes(this.currentUser.cusuario)){
+      this.menuItems = [
+        {
+          text: 'Generales', icon: 'fa-solid fa-gear' , id: 'generales', items: [
+            {text: 'Países', icon: 'fa-solid fa-flag', url: 'paises'},
+            {text: 'Estados', icon: 'fa-solid fa-road', url: 'estados'},
+            {text: 'Ciudades', icon: 'fa-solid fa-map-location-dot', url: 'ciudades'},
+          ]
+        },
+        {
+          text: 'Empresas', icon: 'fa-solid fa-building' , id: 'empresas', items: [
+            {text: 'Bancos', icon: 'fa-solid fa-building-columns', url: 'bancos'},
+            {text: 'Cedentes', icon: 'fa-solid fa-briefcase', url: 'cedentes'},
+            {text: 'Monedas', icon: 'fa-solid fa-coins', url: 'monedas'},
+          ]
+        },
+        {
+          text: 'Personas', icon: 'fa-solid fa-user' , id: 'personas', items: [
+            {text: 'Clientes', icon: 'fa-solid fa-id-card', url: 'clientes'},
+            {text: 'Productores', icon: 'fa-solid fa-people-arrows', url: 'productores'},
+            {text: 'Usuarios', icon: 'fa-solid fa-circle-user', url: 'usuarios'},
+          ]
+        },
+        {
+          text: 'Del Negocio', icon: 'fa-solid fa-briefcase' , id: 'negocio', items: [
+            {text: 'Vehículos', icon: 'fa-solid fa-car-side', url: 'vehiculos'},
+            {text: 'Ramos', icon: 'fa-solid fa-file-lines', url: 'ramos'},
+            {text: 'Productos', icon: 'fa-solid fa-scale-balanced', url: 'productos'},
+            {text: 'Métodologías de Pago', icon: 'fa-solid fa-comments-dollar', url: 'metodologiapago'},
+          ]
+        },
+      ]
+    }
   }
 
   collapse(){
